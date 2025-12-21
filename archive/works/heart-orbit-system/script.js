3fd32c48503ad81add35190057fad7c75d5f37af
jungsjnyu <sjung220@pratt.edu>
2025-12-20 19:16:13 -0500

    1

--- archive/works/heart-orbit-system/script.js
+++ archive/works/heart-orbit-system/script.js
@@ -103,7 +103,7 @@ function isMobileDevice() {
 function isMouseOverCenterHeart() {
   const centerX = windowWidth / 2;
   const centerY = windowHeight / 2;
-  const hoverRange = 110;
+  const hoverRange = 60;
 
   return (
     mouseX >= centerX - hoverRange &&
