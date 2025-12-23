994cfff8881ffd0a2f9720176c094b176e3abae7
jungsjnyu <sjung220@pratt.edu>
2025-12-23 09:01:08 -0500

    1

--- archive/works/a-moth-drawn-to-the-scent-of-peonies/peony-moth.js
+++ archive/works/a-moth-drawn-to-the-scent-of-peonies/peony-moth.js
@@ -93,3 +93,6 @@ function initVideoPlayback() {
   document.addEventListener("touchstart", onUserGesture, { once: true });
   document.addEventListener("click", onUserGesture, { once: true });
 }
+
+
+
