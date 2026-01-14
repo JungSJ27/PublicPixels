df12b85b37a2dd8f99956a0734610eee261d1e96
jungsjnyu <sjung220@pratt.edu>
2026-01-14 17:48:23 -0500

    1

--- about/sj1.js
+++ about/sj1.js
@@ -37,7 +37,7 @@ pmrem.compileEquirectangularShader();
 let sky = null;
 
 new EXRLoader().load(
-  "./citrus_orchard_road_puresky_4k.exr",
+  "./citrus_orchard_road_puresky_1k.exr",
   (tex) => {
     tex.mapping = THREE.EquirectangularReflectionMapping;
 
