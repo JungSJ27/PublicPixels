e3fb211697cefc33b82b364cfe2170ff91b4dc32
jungsjnyu <sjung220@pratt.edu>
2026-03-22 18:38:14 -0400

    1

--- front/search.js
+++ front/search.js
@@ -152,7 +152,7 @@ export function ensureSearchPanel() {
     { title: "SJ1",                 url: "/about/",                             image: "/front/SJ1.png" },
     { title: "Still, Life goes on", url: "/archive/works/still-life-goes-on/",  image: "/front/recstill.png" },
     { title: "Rent-a-JJ Chat",      url: "/pixelshop/shop/rent-a-jjchat/",      image: "/front/jjchat.png" },
-    { title: "Brooklyn to Gangnam Magazine Volume 2", url: "/studiolog/log/202603/",   image: "/front/0315.png" },
+    { title: "Brooklyn to Gangnam Magazine Volume 2", url: "/studiolog/log/202603.N2/",   image: "/front/0315.png" },
   ];
 
   function renderDemo() {
